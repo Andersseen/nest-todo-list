1e67dc8da14c226f852984efdad007e0bdb0ce52
Andrey <forestx24@gmail.com>
2022-05-21 19:48:44 +0200

    conect in DB

---
+++ src/modules/todo/todo.module.ts
@@ -0,0 +1,11 @@
+import { Todo } from './entities/todo.entity';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
+
+  controllers: [],
+  providers: [],
+})
+export class TodoModule {}
